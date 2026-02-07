1aa77aea7df1b9b1eec6ec5f52c5eee7ea0a91ff
Farzad Azimipour <farzad.azimipoor@gmail.com>
2026-02-07 12:33:52 +0000

    Added required libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,21 +46,14 @@ dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.ui)
 
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewModel.navigation3)
-    implementation(libs.androidx.navigation3.ui)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.viewModel.navigation3)
     implementation(libs.kotlinx.serialization.json)
-
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     testImplementation(libs.junit)
 
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.test.espresso.core)
 }
